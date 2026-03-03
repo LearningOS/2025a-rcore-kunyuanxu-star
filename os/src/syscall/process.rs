964315913fe5a3d8df65b9063986be377e968973
xukunyuan <xky13938334565@163.com>
2026-03-03 17:33:36 +0800

    feat(任务管理): 添加系统调用统计功能
    在任务控制块中增加系统调用计数数组，并实现相关统计方法
    添加 sys_trace 系统调用实现，支持查询系统调用次数

--- os/src/syscall/process.rs
+++ os/src/syscall/process.rs
@@ -1,6 +1,6 @@
 //! Process management syscalls
 use crate::{
-    task::{exit_current_and_run_next, suspend_current_and_run_next},
+    task::{exit_current_and_run_next, suspend_current_and_run_next, TASK_MANAGER},
     timer::get_time_us,
 };
 
@@ -38,8 +38,34 @@ pub fn sys_get_time(ts: *mut TimeVal, _tz: usize) -> isize {
     0
 }
 
-// TODO: implement the syscall
-pub fn sys_trace(_trace_request: usize, _id: usize, _data: usize) -> isize {
+/// trace system call
+pub fn sys_trace(trace_request: usize, id: usize, data: usize) -> isize {
     trace!("kernel: sys_trace");
-    -1
+    match trace_request {
+        0 => {
+            unsafe {
+                let ptr = id as *const u8;
+                if ptr.is_null() {
+                    -1
+                } else {
+                    core::ptr::read_volatile(ptr) as isize
+                }
+            }
+        }
+        1 => {
+            unsafe {
+                let ptr = id as *mut u8;
+                if ptr.is_null() {
+                    -1
+                } else {
+                    core::ptr::write_volatile(ptr, data as u8);
+                    0
+                }
+            }
+        }
+        2 => {
+            TASK_MANAGER.get_syscall_count(id) as isize
+        }
+        _ => -1,
+    }
 }
--- os/src/task/mod.rs
+++ os/src/task/mod.rs
@@ -54,6 +54,7 @@ lazy_static! {
         let mut tasks = [TaskControlBlock {
             task_cx: TaskContext::zero_init(),
             task_status: TaskStatus::UnInit,
+            syscall_counts: [0; 512],
         }; MAX_APP_NUM];
         for (i, task) in tasks.iter_mut().enumerate() {
             task.task_cx = TaskContext::goto_restore(init_app_cx(i));
@@ -133,6 +134,26 @@ impl TaskManager {
             // go back to user mode
         } else {
             panic!("All applications completed!");
+        }
+    }
+
+    /// Increment system call count for the current task
+    pub fn increment_syscall_count(&self, syscall_id: usize) {
+        let mut inner = self.inner.exclusive_access();
+        let current = inner.current_task;
+        if syscall_id < 512 {
+            inner.tasks[current].syscall_counts[syscall_id] += 1;
+        }
+    }
+
+    /// Get system call count for the current task
+    pub fn get_syscall_count(&self, syscall_id: usize) -> usize {
+        let inner = self.inner.exclusive_access();
+        let current = inner.current_task;
+        if syscall_id < 512 {
+            inner.tasks[current].syscall_counts[syscall_id]
+        } else {
+            0
         }
     }
 }
